e44e2ca4edc5ffe3c54da2fd7d70de3691aac2e3
Bruno Thalmann <mr.thalmann@gmail.com>
2015-04-22 10:33:35 +0200

    implemented new interface

--- app/src/main/java/dk/aau/cs/psylog/survey_library/Task.java
+++ app/src/main/java/dk/aau/cs/psylog/survey_library/Task.java
@@ -3,10 +3,15 @@ package dk.aau.cs.psylog.survey_library;
 import android.app.Notification;
 
 public abstract class Task implements IScheduled{
+    private Long time;
 
     @Override
-    public Long getNextTime() {
-        return null;
+    public Long getTime() {
+        return time;
+    }
+
+    @Override
+    public void updateTime() {
     }
 
     @Override
