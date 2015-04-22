5479826e68e34228c8839d18fa3bb245180ecbd4
Bruno Thalmann <mr.thalmann@gmail.com>
2015-04-22 08:49:59 +0200

    an abstract task

--- app/src/main/java/dk/aau/cs/psylog/survey_library/Task.java
+++ app/src/main/java/dk/aau/cs/psylog/survey_library/Task.java
@@ -1,4 +1,16 @@
 package dk.aau.cs.psylog.survey_library;
 
-public class Task {
+import android.app.Notification;
+
+public abstract class Task implements IScheduled{
+
+    @Override
+    public Long getNextTime() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotification() {
+        return null;
+    }
 }
