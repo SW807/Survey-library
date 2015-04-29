2f36c30c8b42b30275fd1b591a3ab63afb6cf081
Bruno Thalmann <mr.thalmann@gmail.com>
2015-04-29 10:35:46 +0200

    removed questiontype from constructor and added time field

--- app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/NumberRangeQuestion.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/NumberRangeQuestion.java
@@ -1,17 +1,21 @@
 package dk.aau.cs.psylog.sensor.survey_library;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
-public class NumberRangeQuestion extends Question{
+import java.util.Calendar;
+
+public class NumberRangeQuestion extends Question {
     private int min;
     private int max;
     private String minLabel;
     private String maxLabel;
+    private Long time;
 
-    public NumberRangeQuestion(String text, QuestionType questionType, int min, int max, String minLabel, String maxLabel) {
-        super(text, questionType);
+    public NumberRangeQuestion(String text, int min, int max, String minLabel, String maxLabel) {
+        super(text, QuestionType.NUMBER_RANGE);
         this.min = min;
         this.max = max;
         this.minLabel = minLabel;
