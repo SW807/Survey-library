7561df48bc7e7a9df02bfe04bf6289ea1e27fb03
Stefan Micheelsen <stufkan@gmail.com>
2015-05-08 13:00:36 +0200

    fixed positions on label on numberrangedialog

--- app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/NumberRangeDialog.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/NumberRangeDialog.java
@@ -7,6 +7,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
