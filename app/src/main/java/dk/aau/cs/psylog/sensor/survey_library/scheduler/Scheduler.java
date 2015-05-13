2cc775b8cac587b04950632c2b84c05d9be2f7d7
Bruno Thalmann <mr.thalmann@gmail.com>
2015-05-13 16:28:26 +0200

    added questions for doing stemningsregistrerings method for proof of concept

--- app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/scheduler/Scheduler.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/survey_library/scheduler/Scheduler.java
@@ -35,22 +35,34 @@ public class Scheduler implements ISensor {
     }
 
     private void initialize() {
-        boolean initializeDB = true;
+        boolean initializeDB = false;
 
         DatabaseHelper databaseHelper = new DatabaseHelper(context);
 
         if (initializeDB) {
+            // #################################################################
+            //Test spørgsmål
+            //MultipleChoiceQuestion multipleChoiceQuestion = new MultipleChoiceQuestion("Her er et multiplechoice spørgsmål, vælg en", true, new String[]{"Valgmulighed 1", "Valgmulighed 2"},0, new QuestionTime("16:05", 1, 0, 0));
+            //MultipleChoiceQuestion multipleChoiceQuestion2 = new MultipleChoiceQuestion("Her er et multiplechoice spørgsmål, vælg flere.", false, new String[]{"Valgmulighed 1", "Valgmulighed 2","Valgmulighed 3", "Valgmulighed 4"},1, new QuestionTime("07:00", 6*60, 6, 21));
+            //PlainTextQuestion plaintext1 = new PlainTextQuestion("Tekst spørgsmål, eksempelvis dagbog", 2, new QuestionTime("14:55", 1, 0, 0));
+            //NumberRangeQuestion numberRangeQuestion = new NumberRangeQuestion("Spørgsmål med skala", 1, 10, "Laveste mulighed", "Højeste mulighed",3, new QuestionTime("07:00", 6*60, 6, 21));
+            //databaseHelper.addQuestion(multipleChoiceQuestion);
+            //databaseHelper.addQuestion(multipleChoiceQuestion2);
+            //databaseHelper.addQuestion(plaintext1);
+            //databaseHelper.addQuestion(numberRangeQuestion);
+            // #################################################################
 
-            MultipleChoiceQuestion multipleChoiceQuestion = new MultipleChoiceQuestion("Her er et multiplechoice spørgsmål, vælg en", true, new String[]{"Valgmulighed 1", "Valgmulighed 2"},0, new QuestionTime("20:00", 0, 0, 0));
-            MultipleChoiceQuestion multipleChoiceQuestion2 = new MultipleChoiceQuestion("Her er et multiplechoice spørgsmål, vælg flere.", false, new String[]{"Valgmulighed 1", "Valgmulighed 2","Valgmulighed 3", "Valgmulighed 4"},1, new QuestionTime("07:00", 6*60, 6, 21));
-            PlainTextQuestion plaintext1 = new PlainTextQuestion("Tekst spørgsmål, eksempelvis dagbog", 2, new QuestionTime("07:00", 13*60, 7, 20));
-            NumberRangeQuestion numberRangeQuestion = new NumberRangeQuestion("Spørgsmål med skala", 1, 10, "Laveste mulighed", "Højeste mulighed",3, new QuestionTime("07:00", 6*60, 6, 21));
-
-
-            databaseHelper.addQuestion(multipleChoiceQuestion);
-            databaseHelper.addQuestion(multipleChoiceQuestion2);
-            databaseHelper.addQuestion(plaintext1);
-            databaseHelper.addQuestion(numberRangeQuestion);
+            //##################################################################
+            //Stemningsregistrering:
+            MultipleChoiceQuestion stemningsleje = new MultipleChoiceQuestion("Vælg dit stemningsleje:", true, new String[]{"Svær mani", "Moderat mani", "Let mani", "Normalt stemningsleje", "Let depression", "Moderat depression", "Svær depression"}, 0, new QuestionTime("07:00", 24, 0, 0));
+            NumberRangeQuestion angst = new NumberRangeQuestion("Angstniveau:", 0, 3, "Ingen angst", "Udtalt angst", 0, new QuestionTime("07:00", 24, 0, 0));
+            NumberRangeQuestion antalTimerSøvn = new NumberRangeQuestion("Antal timer søvn:", 0, 14, "0 timer", "14 timer", 0, new QuestionTime("07:00", 24, 0, 0));
+            PlainTextQuestion notaterOgVægt = new PlainTextQuestion("Angiv notater og vægt(mindst en gang om måneden)", 0, new QuestionTime("07:00", 24, 0, 0));
+            databaseHelper.addQuestion(stemningsleje);
+            databaseHelper.addQuestion(angst);
+            databaseHelper.addQuestion(antalTimerSøvn);
+            databaseHelper.addQuestion(notaterOgVægt);
+            // #################################################################
         }
         tasks.addAll(databaseHelper.getQuestions());
     }
